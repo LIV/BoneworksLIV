068ed2d04435b7b19448853107fb412d23bec19c
Raicuparta <Raicuparta@users.noreply.github.com>
2022-05-30 14:41:24 +0800

    create shader cache

--- BoneworksLIV/AssetManager.cs
+++ BoneworksLIV/AssetManager.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 using System.IO;
+using MelonLoader;
 using UnityEngine;
 
 namespace BoneworksLIV
 {
 	public static class AssetManager
 	{
-		private static readonly string assetsDir =  MelonLoader.MelonUtils.UserDataDirectory + "/LIVAssets/";
+		private static readonly string assetsDir = @"C:\Users\ricar\AppData\Roaming\r2modmanPlus-local\BONEWORKS\profiles\Default\Mods\LIVAssets\";
 
 		public static AssetBundle LoadBundle(string assetName)
 		{
-			var bundle = AssetBundle.LoadFromFile(assetsDir +	assetName);
-
+			var bundlePath = Path.Combine(assetsDir, assetName);
+			// var bundle = AssetBundle.LoadFromFile(Path.Combine(MelonLoader.MelonUtils.BaseDirectory, assetsDir, assetName));
+			var bundle = AssetBundle.LoadFromFile(bundlePath);
+			
+			MelonLogger.Msg("### Loading bundle from " + bundlePath);
+				
 			if (bundle == null)
 			{
-				throw new Exception("Failed to load asset bundle " + assetName);
+				throw new Exception("Failed to load asset bundle " + bundlePath);
 			}
 			
 			return bundle;
---
+++ BoneworksLIV/BoneworksLivMod.cs
@@ -0,0 +1,37 @@
+﻿
+using LIV.SDK.Unity;
+using MelonLoader;
+using UnhollowerBaseLib.Runtime;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+
+namespace BoneworksLIV
+{
+	public class BoneworksLivMod: MelonMod
+	{
+		public override void OnApplicationStart()
+		{
+			base.OnApplicationStart();
+			var livAssetBundle = AssetManager.LoadBundle("liv-shaders");
+			// var shaders = livAssetBundle.LoadAll<Shader>();
+			
+			MelonLogger.Msg("### livAssetBundle exists? " + (livAssetBundle != null));
+			
+			SDKShaders.LoadFromAssetBundle(livAssetBundle);
+
+		}
+
+		public override void OnUpdate()
+		{
+			base.OnUpdate();
+			if (Input.GetKeyDown(KeyCode.L))
+			{
+				var livAssetBundle = AssetManager.LoadBundle("liv-shaders");
+
+				SDKShaders.LoadFromAssetBundle(livAssetBundle);
+				// var shaders = livAssetBundle.LoadAll<Shader>();
+				// SDKShaders.LoadFromAssetBundle(AssetManager.LoadBundle("liv-shaders"));
+			}
+		}
+	}
+}
