091895d511018807ab1399eb0a83d9f4bd2ced44
Raicuparta <Raicuparta@users.noreply.github.com>
2022-05-30 10:53:27 +0800

    Add unity project

---
+++ BoneworksLIV/BoneworksLivPlugin.cs
@@ -0,0 +1,12 @@
+﻿using MelonLoader;
+
+namespace BoneworksLIV
+{
+	public class BoneworksLivPlugin: MelonMod
+	{
+		public override void OnApplicationStart()
+		{
+			base.OnApplicationStart();
+		}
+	}
+}
---
+++ BoneworksLIV/Patches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Steamworks;
+
+namespace BoneworksLIV
+{
+	[HarmonyPatch]
+	public class Patches
+	{
+		[HarmonyPrefix]
+		[HarmonyPatch(typeof(SteamAPI), "RestartAppIfNecessary")]
+		// TODO definitely don't include this in the mod.
+		private static bool SkipSteamCheck()
+		{
+			return false;
+		}
+	}
+}
